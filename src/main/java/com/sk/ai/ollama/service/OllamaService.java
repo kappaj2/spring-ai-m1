1f57929f8120341c6bb1298feddcc2f5a2127996
Andre Kapp <kappaj@gmail.com>
2024-07-08 22:06:04 +0400

    Getting ollama up and running

--- src/main/java/com/sk/ai/ollama/service/OllamaService.java
+++ src/main/java/com/sk/ai/ollama/service/OllamaService.java
@@ -1,28 +1,45 @@
 package com.sk.ai.ollama.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptionsBuilder;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class OllamaService {
 
-     private final OllamaChatModel ollamaChatModel;
+     //private  OllamaChatModel ollamaChatModel;
+     private ChatClient chatClient;
+
+     public OllamaService(OllamaChatModel ollamaChatModel) {
+          // this.ollamaChatModel = ollamaChatModel;
+          ChatClient.Builder builder = ChatClient.builder(ollamaChatModel)
+                  .defaultOptions(ChatOptionsBuilder.builder()
+                          .withTemperature(0.0F)
+                          .build());
+          this.chatClient = builder.build();
+     }
+
+
+//     public OllamaService(OllamaChatModel ollamaChatModel) {
+//          this.ollamaChatModel = ollamaChatModel;
+//     }
 
      public String generate(String promptMessage) {
-          ChatResponse response = ollamaChatModel.call(
-                  new Prompt(promptMessage,
-                          OllamaOptions.create()
-                                  .withModel("llama3")
-                                  .withTemperature(0.4F)
-                  )
-          );
-          return response.getResult().getOutput().getContent();
+
+//          ChatResponse response = ollamaChatModel.call(prompt);
+//          return response.getResult().getOutput().getContent();
+
+
+          return chatClient
+                  .prompt()
+                  .system("""
+                                      Tell the joke in the style of a cowboy
+                          """)
+                  .user(promptMessage)
+                  .call()
+                  .content();
      }
 }
